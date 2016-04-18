4f5373ba3f7c94a66270042b63b4c0974ad63570
Mireia <mireia.gbedmar@gmail.com>
2016-04-19 00:58:52 +0200

    DomainException

---
+++ src/main/java/Domain/DomainException.java
@@ -0,0 +1,23 @@
+package Domain;
+
+
+public class DomainException extends Exception {
+    private String friendlyMessage;
+
+    public DomainException() {
+        super();
+    }
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(String message, String friendlyMessage) {
+        super(message);
+        this.friendlyMessage = friendlyMessage;
+    }
+
+    public String getFriendlyMessage() {
+        return this.friendlyMessage;
+    }
+}
