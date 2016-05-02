a6ddded338a6c046c44012ffff26237e2a289d13
Mireia <mireia.gbedmar@gmail.com>
2016-05-03 01:35:50 +0200

    test per persistencia

--- src/test/ParserTest/Controller.java
+++ src/test/ParserTest/Controller.java
@@ -2,17 +2,22 @@ package ParserTest;
 
 import GraphTest.Graph.*;
 
+import java.io.IOException;
+import java.util.Set;
+
 
 public class Controller {
-    private PersistenceLineByLine pers;
+    public GraphFileManager pers;
     public Graph g;
 
     public Controller() {
         g = new Graph();
-        pers = new PersistenceLineByLine(null);
+        pers = new GraphFileManager();
     }
 
-    public void importGraph() throws DomainException {
+    public void importGraph() throws DomainException, IOException, PersistenceException {
+        pers.startLoading(".");
+
         String[] elem;
 
         //Nodes
@@ -51,6 +56,76 @@ public class Controller {
             Node b = g.getNode(Integer.parseInt(elem[1]), "conf");
             g.addEdge(a, b);
         }
+    }
+
+    public void saveGraph(String path) throws IOException, DomainException, PersistenceException {
+        pers.startSaving(path);
+        Set<Node> s = g.getSetOfNodes(Author.TYPE);
+        for(Node n : s)
+        {
+            String[] line = {String.valueOf(n.getID()), n.getName()};
+            System.out.println(n.getName());
+            pers.addAuthor(line);
+        }
+        pers.addAuthor(null);
+        System.out.println("Fet!");
+
+        s = g.getSetOfNodes(Term.TYPE);
+        for(Node n : s)
+        {
+            String[] line = {String.valueOf(n.getID()), n.getName()};
+            pers.addTerm(line);
+        }
+        pers.addTerm(null);
+        System.out.println("Fet!");
+
+        s = g.getSetOfNodes(Conf.TYPE);
+        for(Node n : s)
+        {
+            String[] line = {String.valueOf(n.getID()), n.getName()};
+            pers.addConf(line);
+        }
+        pers.addConf(null);
+        System.out.println("Fet!");
+
+        s = g.getSetOfNodes(Paper.TYPE);
+        for(Node n : s)
+        {
+            String[] line = {String.valueOf(n.getID()), n.getName()};
+            pers.addPaper(line);
+        }
+        pers.addPaper(null);
+        System.out.println("Fet!");
+
+        for (Node n : s) {
+            for (Node vei : g.getNeighbours(n, Author.TYPE)) {
+                String[] line = {String.valueOf(n.getID()), String.valueOf(vei.getID())};
+                pers.addPaperAuthor(line);
+            }
+        }
+        pers.addPaperAuthor(null);
+        System.out.println("Fet!");
+
+        for (Node n : s) {
+            for (Node vei : g.getNeighbours(n, Term.TYPE)) {
+                String[] line = {String.valueOf(n.getID()), String.valueOf(vei.getID())};
+                pers.addPaperTerm(line);
+            }
+        }
+        pers.addPaperTerm(null);
+        System.out.println("Fet!");
+
+        for (Node n : s) {
+            for (Node vei : g.getNeighbours(n, Conf.TYPE)) {
+                String[] line = {String.valueOf(n.getID()), String.valueOf(vei.getID())};
+                pers.addPaperConf(line);
+            }
+        }
+        pers.addPaperConf(null);
+        System.out.println("Fet!");
+
+        pers.commit();
+
     }
 
 }
---
+++ src/test/ParserTest/PersistenceException.java
@@ -0,0 +1,19 @@
+package ParserTest;
+;
+
+/**
+ * @author Gerard
+ */
+public class PersistenceException extends AbstractException {
+    public PersistenceException() {
+        super();
+    }
+
+    public PersistenceException(String message) {
+        super(message);
+    }
+
+    public PersistenceException(String message, String friendlyMessage) {
+        super(message, friendlyMessage);
+    }
+}
