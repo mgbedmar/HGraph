9d18033b71c121e38f31748fc32d6cc04a23fd00
Gerard del Castillo <gerarddelcasti@hotmail.com>
2016-05-02 22:54:56 +0200

    working on domaincontroller

--- src/main/java/Domain/DomainController.java
+++ src/main/java/Domain/DomainController.java
@@ -3,6 +3,7 @@ package Domain;
 
 import Domain.Graph.*;
 import Persistence.PersistenceController;
+import Persistence.PersistenceException;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -336,6 +337,39 @@ public class DomainController
      */
     public void resetResult() {
         r.resetIndex();
+    }
+
+    /**
+     * Retorna la llista de projectes guardats
+     * @return
+     */
+    public String[] getProjectList(){
+        return pc.getProjectList();
+    }
+
+    public void selectProject(String pn) throws DomainException {
+        try
+        {
+            pc.selectProject(pn);
+        }
+        catch (PersistenceException e)
+        {
+            throw new DomainException("Hi ha hagut un problema al intentar seleccionar el projecte: "+e.getFriendlyMessage());
+        }
+    }
+
+    public void save(){
+        String[] types = {
+                Author.TYPE,
+                Paper.TYPE,
+                Term.TYPE,
+                Conf.TYPE
+        };
+        for(String t : types)
+        {
+            
+        }
+
     }
 
 }
--- src/main/java/Persistence/PersistenceController.java
+++ src/main/java/Persistence/PersistenceController.java
@@ -4,4 +4,16 @@ package Persistence;
 public class PersistenceController {
     private GraphFileManager gfm;
     private ProjectManager pm;
+
+    public String[] getProjectList() {
+        return pm.getProjectList();
+    }
+
+    public void selectProject(String pn) throws PersistenceException {
+        pm.selectProject(pn);
+    }
+
+    public void save() {
+
+    }
 }
