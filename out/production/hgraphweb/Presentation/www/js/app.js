9651f834874d0f7606e522afa59e93f68c1d39e0
Mireia <mireia.gbedmar@gmail.com>
2016-05-20 01:59:23 +0200

    trets xivatos

--- out/production/hgraphweb/Presentation/www/js/app.js
+++ out/production/hgraphweb/Presentation/www/js/app.js
@@ -94,7 +94,7 @@ function loadGraph(){
 
 function initLoadPage(){
     var myList = document.getElementById('projectList');
-    //myList.innerHTML = '';
+    myList.innerHTML = '';
     var projects = window.HGraph.getProjects();
     projects.forEach(function(e){
         var child = document.createElement("li");
@@ -113,48 +113,43 @@ function drawGraph(cb){
             nodes: [],
             edges: []
         };
-    window.HGraph.log("asdf");
+
     var nodes = window.HGraph.getNodesOfType("paper");
-    //window.HGraph.log(nodes);
-    window.HGraph.log("asf2");
-    window.HGraph.log(typeof nodes)
-    /*nodes.forEach(function(e){
-        g.nodes.push({
-            id: e[0],
-            label: e[1],
-            x: Math.random()/10,
-            y: Math.random()/10,
-            size: Math.random(),
-            color: "red"
-        });
-    });*/
-    window.HGraph.log(typeof nodes.get(0)[0]);
+
     for (var i = 0; i < nodes.size(); i++) {
         g.nodes.push({
             id: String(nodes.get(i)[0]),
             label: String(nodes.get(i)[1]),
-            x: Math.random()/10,
-            y: Math.random()/10,
+            x: Math.random(),
+            y: Math.random(),
             size: Math.random()
         });
     }
-
-    window.HGraph.log("abans de sigma");
-
 
     var s = new sigma({
         graph: g,
         container: 'graph-container',
         settings: {
-            minNodeSize: 2,
+            minNodeSize: 1,
             maxNodeSize: 4,
-            minEdgeSize: 1,
-            maxEdgeSize: 1,
-            eventsEnabled: false
+            minEdgeSize: 0.2,
+            maxEdgeSize: 0.5,
+            eventsEnabled: true,
+            minRatio: 10, // How far can we zoom out?
+            maxRatio: 20, // How far can we zoom in?
+            defaultLabelColor: "#000",
+            defaultLabelSize: 14,
+            defaultLabelBGColor: "#ddd",
+            defaultHoverLabelBGColor: "#002147",
+            defaultLabelHoverColor: "#fff",
+            labelThreshold: 10,
+            defaultEdgeType: "curve",
+            hoverFontStyle: "bold",
+            fontStyle: "bold",
+            activeFontStyle: "bold"
         }
     });
-
-    window.HGraph.log("hey");
+    
     cb();
 }
 
